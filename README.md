eaac360acdcbb42b7b290325c4d736588f932517
ivan89898 <chizhevskiy.ivan@mail.ru>
2022-08-21 01:26:14 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/fj7q5cenkjrd3afg?svg=true)](https://ci.appveyor.com/project/ivan89898/selen)
---
+++ src/test/java/AppCardDeliveryTest.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AppCardDeliveryTest {
+    @Test
+    void ShouldTestV1() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Екатеринбург");
+        $(By.xpath("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/input")).sendKeys(Keys.CONTROL + "a");
+        $(By.xpath("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/input")).sendKeys(Keys.DELETE);
+        $(By.xpath("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/input")).setValue("25082022");
+        $("[data-test-id=name] input").setValue("Иван Иванов");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $(By.xpath("//*[text()='Забронировать']")).click();
+        $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+}
