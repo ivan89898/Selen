d70a1b89c0ace5bac4f1ef67219eaf6c654863c0
ivan89898 <chizhevskiy.ivan@mail.ru>
2022-08-21 02:30:28 +0500

    Test

--- src/test/java/AppCardDeliveryTest.java
+++ src/test/java/AppCardDeliveryTest.java
@@ -1,7 +1,13 @@
 import com.codeborne.selenide.Condition;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
@@ -9,6 +15,22 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class AppCardDeliveryTest {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
     @Test
     void ShouldTestV1() {
         open("http://localhost:9999");
@@ -21,5 +43,6 @@ public class AppCardDeliveryTest {
         $("[data-test-id=agreement]").click();
         $(By.xpath("//*[text()='Забронировать']")).click();
         $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
+
     }
 }
