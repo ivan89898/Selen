87693f1fd202bd1ac54ea4115be313aa63ee784f
ivan89898 <chizhevskiy.ivan@mail.ru>
2022-08-27 01:35:28 +0500

    positiv tests

--- src/test/java/AppCardDeliveryTest.java
+++ src/test/java/AppCardDeliveryTest.java
@@ -1,40 +1,57 @@
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class AppCardDeliveryTest {
-    @Test
-    void ShouldTestV1() {
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    String planningDate = generateDate(4);
+
+    {
         open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldTestV1() {
         $("[data-test-id=city] input").setValue("Екатеринбург");
-        $(By.xpath("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/input")).sendKeys(Keys.CONTROL + "a");
-        $(By.xpath("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/input")).sendKeys(Keys.DELETE);
-        $(By.xpath("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/input")).setValue("25082022");
+        $("[data-test-id=date] input.input__control").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input.input__control").sendKeys(Keys.DELETE);
+        $("[data-test-id=date] input.input__control").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Иван Иванов");
         $("[data-test-id=phone] input").setValue("+79999999999");
         $("[data-test-id=agreement]").click();
         $(By.xpath("//*[text()='Забронировать']")).click();
         $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
-
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
+
     @Test
-    void ShouldTestV2() {
-        open("http://localhost:9999");
+    void shouldTestV2() {
         $("[data-test-id=city] input").setValue("Екатеринбург");
-        $(By.xpath("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/input")).sendKeys(Keys.CONTROL + "a");
-        $(By.xpath("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/input")).sendKeys(Keys.DELETE);
-        $(By.xpath("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/input")).setValue("25082022");
-        $("[data-test-id=name] input").setValue("Ёжин Иванов");
+        $("[data-test-id=date] input.input__control").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input.input__control").sendKeys(Keys.DELETE);
+        $("[data-test-id=date] input.input__control").setValue(planningDate);
+        $("[data-test-id=name] input").setValue("ежин Иванов");
         $("[data-test-id=phone] input").setValue("+79999999999");
         $("[data-test-id=agreement]").click();
         $(By.xpath("//*[text()='Забронировать']")).click();
         $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
     }
 }
